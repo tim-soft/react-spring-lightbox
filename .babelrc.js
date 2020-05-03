1f4ff7fa5099a09b309c164bf298b6e3e69fe665
Tim Ellenberger <tim.ellenberger@liveauctioneers.com>
2020-05-03 15:56:31 -0600

    Minify styled-components styles and output bundles

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: ['@babel/env', '@babel/react'],
     plugins: [
+        ['babel-plugin-styled-components'],
         ['@babel/plugin-proposal-class-properties'],
         ['@babel/plugin-proposal-object-rest-spread'],
         ['transform-react-remove-prop-types', { removeImport: true }],
