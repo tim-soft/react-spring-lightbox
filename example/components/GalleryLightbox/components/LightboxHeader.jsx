95af9dea04a83f6a59e65e6ab670357892620090
Jaron Earle <jaronearle@ATGL-D17C2L069T.local>
2026-07-21 15:11:59 -0600

    fix example app styles

--- example/components/GalleryLightbox/components/LightboxHeader.jsx
+++ example/components/GalleryLightbox/components/LightboxHeader.jsx
@@ -69,6 +69,8 @@ const RightSideContainer = styled.div`
 const CloseButton = styled(ButtonControl)`
     height: 100%;
     display: flex;
+    align-items: center;
+    justify-content: center;
     border-left-style: solid;
     border-left-width: 3px;
     border-left-color: ${({ theme }) => theme.headerNavFontColor};
